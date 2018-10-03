3176a281ece1563677cb260b5c40758a9c08e606
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2018-10-03 11:04:51 +0200

    source added

--- loadingployfil/index.js
+++ loadingployfil/index.js
@@ -1,3 +1,6 @@
+// Taken from Google chrome developers channel
+// https://www.youtube.com/watch?v=RoVy9EoIloQ&t=0s&list=PLNYkxOF6rcIAKIQFsNbV0JDws_G_bnNo9&index=3
+
 let TransformStreamPromise = Promise.resolve()
     .then(() => {
         if ('TransfromStream' in self) { // check if it is available, self maybe window or module you are working with 
