fbb792ce416995291dd917b00dc404f61318949f
Madhu Rakhal Magar <rakhal.madhu@gmail.com>
2018-10-03 10:57:39 +0200

    commented

--- loadingployfil/index.js
+++ loadingployfil/index.js
@@ -1,8 +1,9 @@
 let TransformStreamPromise = Promise.resolve()
     .then(() => {
-        if ('TransfromStream' in self) { // check if it is available
+        if ('TransfromStream' in self) { // check if it is available, self maybe window or module you are working with 
             return TransfromStream;
         }
+        // have to load polyfill because it is not avaiable
         const {TransfromStream} = await import('/streams-module.js');
         return TransfromStream;
     });
